cdd4db0dfa695194be9d78fe2af663139e3aaf48
Nishida Ryosuke <55966582+24daryo@users.noreply.github.com>
2023-12-03 12:18:21 +0900

    update

--- src/app/posts/[slug]/layout.tsx
+++ src/app/posts/[slug]/layout.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    import("zenn-embed-elements"); // 数式をブラウザでレンダリングできるようにする
+    import("zenn-embed-elements"); // 数式をブラウザでレンダリングできるように
   });
   return <>{children}</>;
 }
