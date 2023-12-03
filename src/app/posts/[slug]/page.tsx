d2c74aa66ed72c62f9f16b84fc8622c773a3523a
Nishida Ryosuke <55966582+24daryo@users.noreply.github.com>
2023-12-03 12:29:57 +0900

    Statioc Site Generationに変更

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,3 +1,5 @@
+// Static Site Generation
+
 import fs from "fs";
 import path from "path";
 import markdownToHtml from "zenn-markdown-html";
@@ -5,6 +7,20 @@ import matter from "gray-matter";
 import "zenn-content-css";
 
 const POST_PATH = "_posts";
+
+// Static Site Generationの場合、generateStaticParamsを定義する必要がある
+export const generateStaticParams = () => {
+  // ファイルの中身を取得
+  const postsDirectory = path.join(process.cwd(), POST_PATH);
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  // fileNamesからslugを取得
+  return fileNames.map((fileName) => {
+    return {
+      slug: fileName.replace(".md", ""),
+    };
+  });
+};
 
 export default async function Post({ params }: any) {
   // URLのパラメータから該当するファイル名を取得
